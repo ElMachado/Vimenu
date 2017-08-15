eed9d9d168451f92b3c7328dfdb4cb1b66acd5a8
Jesus Arroyo <elmachado001@gmail.com>
2017-08-15 00:18:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/elmac/vimenu/InicioVimenu.java
@@ -0,0 +1,13 @@
+package com.example.elmac.vimenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioVimenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_vimenu);
+    }
+}
