0afa6458b24d580e1a952be65a12c99170f630b7
Jesus Arroyo <elmachado001@gmail.com>
2017-08-16 15:44:34 -0500

    //se creo el diseño de la interfaz pricipal

--- app/src/main/java/com/example/elmac/vimenu/InicioVimenu.java
+++ app/src/main/java/com/example/elmac/vimenu/InicioVimenu.java
@@ -2,6 +2,7 @@ package com.example.elmac.vimenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class InicioVimenu extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class InicioVimenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_vimenu);
+
     }
 }
