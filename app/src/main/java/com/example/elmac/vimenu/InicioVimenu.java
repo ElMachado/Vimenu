8e969cfde6c851752e6ae2dbd5b64b188f41c6a7
Jesus Arroyo <elmachado001@gmail.com>
2017-08-16 19:22:03 -0500

    //Se creo un activity_hubicacion_vimenu y se enlazo al boton ver carta por medio de un metodo intent explicito.

--- app/src/main/java/com/example/elmac/vimenu/InicioVimenu.java
+++ app/src/main/java/com/example/elmac/vimenu/InicioVimenu.java
@@ -1,15 +1,28 @@
 package com.example.elmac.vimenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
+import android.view.View;
 
 public class InicioVimenu extends AppCompatActivity {
-
+    private View btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_vimenu);
 
+        btn=(Button) findViewById(R.id.buttonVerubicacion);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                //acceder al Activity_Hubicacion_vimenu
+                Intent intent=new Intent(InicioVimenu.this,HubicacionVimenu.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
